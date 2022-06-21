260e16408862b165d42e792c8a01733793337101
Elssky <1914127671@qq.com>
2022-06-21 12:09:39 +0800

    123

--- main.cpp
+++ main.cpp
@@ -31,20 +31,19 @@ int main() {
 
 	//preprocess
 	/*format_graph("./dataset/DBLP.txt");*/
+
 	PUNGraph G = TSnap::LoadEdgeList<PUNGraph>("./dataset/CA-AstroPh.txt", 0, 1);
-	printf("G: \n node_nums = %d, edge_nums = %d\n", G->GetNodes(), G->GetEdges());
-	
-	int K_Arr[] = {11};//13, 26, 39, 51 
+	printf("G: \nnode_nums = %d, edge_nums = %d\n", G->GetNodes(), G->GetEdges());
+
+	int K_Arr[] = {45};
 	string filename = "CA-AstroPh_out1.txt";
 	FILE* outFile = fopen(filename.c_str(), "a");
-	//for (int i = K_Arr[0]; ; i += 1) {
+
 	for (int i = 0; i < sizeof(K_Arr) / sizeof(int); i += 1) {
 		int k = K_Arr[i];
-		//string filename = "Cit_k=" + to_string(k) + ".txt";A
-		//int k = i;
+
 		fprintf(outFile, "## dataset: CA-AstroPh k = %d\n", k);
 		printf("k = %d\n", k);
-		
 		
 		int j = 0;
 		/*for (int i = 0; i <= 0; i++) {*/
@@ -56,21 +55,26 @@ int main() {
 		clock_t t1 = clock();
 		TUNGraV VCC1 = VCCE_S.KVCC_ENUM(VCCE_S.G, VCCE_S.k);
 		/*printf("%fs\n", (clock() - t1) * 1.0 / CLOCKS_PER_SEC);*/
-		printf("VCC_Num:%d\n", VCC1.Len());
 		if (VCC1.Len() == 0) break;
-		fprintf(outFile, "VCC_Num:%d\n", VCC1.Len());
-		fprintf(outFile, "time for LOC_CUT(flow>=k): %fs\n", VCCE_S._time2);
-		fprintf(outFile, "Call for LOC_CUT(flow>=k): %d\n", VCCE_S.m2);
-		fprintf(outFile, "time for LOC_CUT(flow<k): %fs\n", VCCE_S._time3);
-		fprintf(outFile, "Call for LOC_CUT(flow<k): %d\n", VCCE_S.m3);
-		fprintf(outFile, "time for LOC_CUT(fake): %fs\n", VCCE_S._time4);
-		fprintf(outFile, "Call for LOC_CUT(fake): %d\n", VCCE_S.m4);
-		printf("time for LOC_CUT(flow>=k): %fs\n", VCCE_S._time2);
-		printf("Call for LOC_CUT(flow>=k): %d\n", VCCE_S.m2);
-		printf("time for LOC_CUT(flow<k): %fs\n", VCCE_S._time3);
-		printf("Call for LOC_CUT(flow<k): %d\n", VCCE_S.m3);
-		printf("time for LOC_CUT(fake): %fs\n", VCCE_S._time4);
-		printf("Call for LOC_CUT(fake): %d\n", VCCE_S.m4);
+
+		fprintf(outFile, "VCC_Num:%d\n"
+			"time for LOC_CUT(flow>=k): %fs\n"
+			"Call for LOC_CUT(flow>=k): %d\n"
+			"time for LOC_CUT(flow<k): %fs\n"
+			"Call for LOC_CUT(flow<k): %d\n"
+			"time for LOC_CUT(fake): %fs\n"
+			"Call for LOC_CUT(fake): %d\n"
+			"Total Time: %fs\n", VCC1.Len(), VCCE_S._time2, VCCE_S.m2, VCCE_S._time3, VCCE_S.m3, VCCE_S._time4, VCCE_S.m4, (clock() - t1) * 1.0 / CLOCKS_PER_SEC);
+
+		printf("VCC_Num:%d\n"
+			"time for LOC_CUT(flow>=k): %fs\n"
+			"Call for LOC_CUT(flow>=k): %d\n"
+			"time for LOC_CUT(flow<k): %fs\n"
+			"Call for LOC_CUT(flow<k): %d\n"
+			"time for LOC_CUT(fake): %fs\n"
+			"Call for LOC_CUT(fake): %d\n"
+			"Total Time: %fs\n", VCC1.Len(), VCCE_S._time2, VCCE_S.m2, VCCE_S._time3, VCCE_S.m3, VCCE_S._time4, VCCE_S.m4, (clock() - t1) * 1.0 / CLOCKS_PER_SEC);
+
 		//VCC1.Sort();
 		/*printf("time for Get_SSV: %fs\n", VCCE_S._time);
 		printf("Call for Get_SSV: %d\n", VCCE_S.m);
